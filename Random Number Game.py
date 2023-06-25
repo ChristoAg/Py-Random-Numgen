312851b56a76b37a1db24f1124225a0d522d1925
Chris <66860407+ChristoAg@users.noreply.github.com>
2023-06-25 22:26:44 +0700

    Update Random Number Game.py

--- Random Number Game.py
+++ Random Number Game.py
@@ -1,16 +1,57 @@
 import random
 
-num1 = int(input("Please select your first number:"))
-num2 = int(input("Please select your second number:"))
+print("Welcome to the Number-guessing Game!")
+choice = input("Please select your difficulty: \n[E] Easy(1-digits) \n[M] Medium(2/3-digits) \n[H] Hard(4-digits) \n[I] Impossible(Up to 6-digits)")
 
-numbers  = random.randint(num1, num2)
-guessednumber = int(input("Guess the number: "))
+def easy():
+    numbers  = random.randint(0, 9)
+    guessednumber = int(input("Guess the number: "))
 
-if guessednumber < numbers:
-    print(f"Sorry, your guess is too low. \nThe correct number is {numbers}")
-elif guessednumber == numbers:
-    print(f"Congratulations! You guessed the number correctly! \nYour guess is {numbers}")
-elif guessednumber > numbers:
-    print(f"Sorry, your guess is too high. \nThe correct number is {numbers}")
-else:
-    print(f"Sorry, the number is out of range. \nRange: {num1} to {num2}")
+    if guessednumber < numbers:
+        print(f"Sorry, your guess is too low. \nThe correct number is {numbers} \nDo you want to try again?")
+    elif guessednumber == numbers:
+        print(f"Congratulations! You guessed the number correctly! \nYour guess is {numbers}")
+    else:
+        print(f"Sorry, your guess is too high. \nThe correct number is {numbers} \nDo you want to try again?")
+
+def medium():
+    numbers1  = random.randint(0, 999)
+    guessednumber1 = int(input("Guess the number: "))
+
+    if guessednumber1 < numbers1:
+        print(f"Sorry, your guess is too low. \nThe correct number is {numbers1} \nDo you want to try again?")
+    elif guessednumber1 == numbers1:
+        print(f"Congratulations! You guessed the number correctly! \nYour guess is {numbers1}")
+    else:
+        print(f"Sorry, your guess is too high. \nThe correct number is {numbers1} \nDo you want to try again?")
+
+def hard():
+    numbers2  = random.randint(0, 9999)
+    guessednumber2 = int(input("Guess the number: "))
+
+    if guessednumber2 < numbers2:
+        print(f"Sorry, your guess is too low. \nThe correct number is {numbers2} \nDo you want to try again?")
+    elif guessednumber2 == numbers2:
+        print(f"Congratulations! You guessed the number correctly! \nYour guess is {numbers2}")
+    else:
+        print(f"Sorry, your guess is too high. \nThe correct number is {numbers2} \nDo you want to try again?")
+
+def impossible():
+    numbers3  = random.randint(0, 999999)
+    guessednumber3 = int(input("Guess the number: "))
+
+    if guessednumber3 < numbers3:
+        print(f"Sorry, your guess is too low. \nThe correct number is {numbers3} \nDo you want to try again?")
+    elif guessednumber3 == numbers3:
+        print(f"Congratulations! You guessed the number correctly! \nYour guess is {numbers3}")
+    else:
+        print(f"Sorry, your guess is too high. \nThe correct number is {numbers3} \nDo you want to try again?")
+
+if choice == "e":
+    easy()
+elif choice == "m":
+    medium()
+elif choice == "h":
+    hard()
+elif choice == "i":
+    impossible()
